5ed659556b4c0b1d1a614094e6f29676c0436cb3
Hodge-zf <xx.skcs@gmail.com>
2017-08-12 16:53:49 -0500

    Variance & standard deviation functions, some slight refactoring as well

--- SpreadsheetPlayground.py
+++ SpreadsheetPlayground.py
@@ -56,8 +56,27 @@ def calculate_mode(numbers):
 	return modes
 
 
-def plot_points(column1, column2):
-	plt.plot(column1, column2, marker='o')
+def find_differences(numbers):
+	mean = calculate_mean(numbers)
+	diff = []
+	for num in numbers:
+		diff.append(num-mean)
+
+	return diff
+
+
+def calculate_variance(numbers):
+	diff = find_differences(numbers)
+	squared_diff = []
+	for d in diff:
+		squared_diff.append(d**2)
+	sum_squared_diff = sum(squared_diff)
+	variance = sum_squared_diff/len(numbers)
+	return variance
+
+
+def plot_points(col1, col2):
+	plt.plot(col1, col2, marker='o')
 	plt.xlabel('Column 1')
 	plt.ylabel('Column 2')
 	plt.title('Visualization of data sets')
@@ -68,26 +87,34 @@ if __name__ == '__main__':
 	column1, column2 = read_spreadsheet(input('Enter filename: '))
 
 	mean1 = calculate_mean(column1)
-	print('The mean of column 1 is: '+str(mean1))
+	print('The mean of column 1 is {0}'.format(str(mean1)))
 
 	median1 = calculate_median(column1)
-	print('The median of column 1 is: '+str(median1))
+	print('The median of column 1 is {0}'.format(str(median1)))
 
 	mode1 = calculate_mode(column1)
-	print('The mode(s) of the list are: ')
+	print('The mode(s) of the column 1 are: ')
 	for mode in mode1:
 		print(mode)
 
+	variance1 = calculate_variance(column1)
+	std1 = variance1**0.5
+	print('The variance of column 1 is {0} and the standard deviation is {1}'.format(variance1, std1))
+
 	mean2 = calculate_mean(column2)
-	print('The mean of column 2 is: '+str(mean2))
+	print('The mean of column 2 is {0}'.format(str(mean2)))
 
 	median2 = calculate_median(column2)
-	print('The median of column 2 is: '+str(median2))
+	print('The median of column 2 is {0}'.format(str(median2)))
 
 	mode2 = calculate_mode(column2)
-	print('The mode(s) of the list are: ')
+	print('The mode(s) of the column 2 are: ')
 	for mode in mode2:
 		print(mode)
+
+	variance2 = calculate_variance(column2)
+	std2 = variance2**0.5
+	print('The variance of column 2 is {0} and the standard deviation is {1}'.format(variance2, std2))
 
 	plot_points(column1, column2)
 
