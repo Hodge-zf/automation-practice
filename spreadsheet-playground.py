2718bdc4c9aafd4de7a894e3bc70cb89c5dd0522
Hodge-zf <xx.skcs@gmail.com>
2017-08-12 13:22:58 -0500

    Mode calculation functionality

--- spreadsheet-playground.py
+++ spreadsheet-playground.py
@@ -1,5 +1,5 @@
 from openpyxl import *
-
+from collections import Counter
 
 def read_spreadsheet(filename):
 	wb = load_workbook(filename)
@@ -43,14 +43,39 @@ def calculate_median(numbers):
 	return median
 
 
+def calculate_mode(numbers):
+	c = Counter(numbers)
+	numbers_freq = c.most_common()
+	max_count = numbers_freq[0][1]
+
+	modes = []
+	for num in numbers_freq:
+		if num[1] == max_count:
+			modes.append(num[0])
+	return modes
+
 
 if __name__ == '__main__':
 	column1, column2 = read_spreadsheet(input('Enter filename: '))
+
 	mean1 = calculate_mean(column1)
 	print('The mean of column 1 is: '+str(mean1))
+
 	median1 = calculate_median(column1)
 	print('The median of column 1 is: '+str(median1))
+
+	mode1 = calculate_mode(column1)
+	print('The mode(s) of the list are: ')
+	for mode in mode1:
+		print(mode)
+
 	mean2 = calculate_mean(column2)
 	print('The mean of column 2 is: '+str(mean2))
+
 	median2 = calculate_median(column2)
 	print('The median of column 2 is: '+str(median2))
+
+	mode2 = calculate_mode(column2)
+	print('The mode(s) of the list are: ')
+	for mode in mode2:
+		print(mode)
